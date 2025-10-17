b77c61d3c7235ae431ad9f5f19e29e9dfe2b0401
Shakawatullah-Tanin <mdshakawatullahtanin23@gmail.com>
2025-10-18 03:24:44 +0600

    make phones data and see dynamicaly  single data from all data

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require ("express")
 
 const app = express()
+const port = 5004;
 
-const port = 5002;
+
+const phones = require("./phone.json");
+
+
 
 app.get("/", (req,res)=>{
 
@@ -10,6 +14,7 @@ app.get("/", (req,res)=>{
     res.send("Hello Server !")
 
 })
+
 app.get("/info",(req,res)=>{
 
 
@@ -28,6 +33,49 @@ app.get("/info",(req,res)=>{
     res.send(info)
 
 })
+
+app.get("/phones", (req,res)=>{
+
+    res.send(phones)
+
+})
+
+
+
+
+
+
+app.get("/phones/:id", (req,res)=>{
+
+
+    const id = parseInt(req.params.id);
+
+    const phone = phones.find(phone=>phone.id===id)
+
+ 
+
+    console.log("i need data for:",id)
+       res.send(phone)
+
+   
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port,()=>{
 
     console.log(`first code running on port :${port}`)
